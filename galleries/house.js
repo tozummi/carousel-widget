b0303a7f19153d8637532854d6c50a6b6e60e32f
tozummi <tozummi@gmail.com>
2026-07-17 20:16:39 +0100

    Update house.js

--- galleries/house.js
+++ galleries/house.js
@@ -1,1 +1,17 @@
-
+const houseGallery = [
+  {
+    src: "galleries/exterior.jpg",
+    caption: "🏡 Ratford Bridge Farmhouse",
+    position: "center"
+  },
+  {
+    src: "galleries/garden.jpg",
+    caption: "🌿 Riverside Garden",
+    position: "bottom"
+  },
+  {
+    src: "galleries/kitchen.jpg",
+    caption: "🍽 Kitchen & Dining Area",
+    position: "center"
+  }
+];
